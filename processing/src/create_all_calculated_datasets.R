18bfc8d2d4ba8801c9eb9cdcbd10448791e46c9e
Zayden Corax <51500317+ZaydenCorax@users.noreply.github.com>
2021-01-17 17:56:59 +0100

    Saisondiagramme v1.1
    removed: Cornichons, Kartoffeln & Steinpilze, da diese in der Saisonquelle fehlen
    updated: Saisondaten und Darstellung
    
    Note: es fehlen noch kleinigkeiten -> Also Daten sichten und auswerten, aber noch nicht unbedingt final einbauen.

--- processing/src/create_all_calculated_datasets.R
+++ processing/src/create_all_calculated_datasets.R
@@ -22,30 +22,30 @@ define_dataset = function(list, columnDefinition, name, annotations) {
   return(list)
 }
 
-define_annotation = function(xmin, xmax, color, alpha){
-  return(list(xmin=xmin, xmax=xmax, color=color, alpha=alpha))
+define_annotation = function(xmin, xmax, alpha){
+  return(list(xmin=xmin, xmax=xmax, color=main_season, alpha=.125*alpha))
 }
 
 create_all_datasets <- function(columnDefinition) {
   returnval <- list()
   
-  returnval = define_dataset(returnval, columnDefinition, "\u00C4pfeln", list(define_annotation(9,12,main_season),define_annotation(1,4,main_season),define_annotation(5,8,lame_season)))
-  returnval = define_dataset(returnval, columnDefinition, "Birnen", list(define_annotation(8,11,main_season, 0.2),define_annotation(12,12,lame_season),define_annotation(1,3,lame_season, 0.2)))
-  returnval = define_dataset(returnval, columnDefinition, "Cornichons", list(define_annotation(7,10,main_season)))
-  returnval = define_dataset(returnval, columnDefinition, "Erbsen", list(define_annotation(7,8,main_season),define_annotation(6,6,lame_season),define_annotation(9,9,lame_season)))
-  returnval = define_dataset(returnval, columnDefinition, "Erdbeeren", list(define_annotation(6,8,main_season),define_annotation(5,5,lame_season),define_annotation(9,9,lame_season)))
-  returnval = define_dataset(returnval, columnDefinition, "Gurken", list(define_annotation(5,9,main_season),define_annotation(3,4,lame_season),define_annotation(10,11,lame_season)))
-  returnval = define_dataset(returnval, columnDefinition, "Himbeeren", list(define_annotation(6,9,main_season),define_annotation(10,10,lame_season)))
-  returnval = define_dataset(returnval, columnDefinition, "Karotten", list(define_annotation(7,12,main_season),define_annotation(1,6,lame_season)))
-  returnval = define_dataset(returnval, columnDefinition, "Kartoffeln", list(define_annotation(1,12,main_season)))
-  returnval = define_dataset(returnval, columnDefinition, "K\u00FCrbissen", list(define_annotation(9,12,main_season),define_annotation(1,1,main_season)))
+  returnval = define_dataset(returnval, columnDefinition, "\u00C4pfeln", list(define_annotation(12,12,4),define_annotation(1,4,4),define_annotation(5,6,3),define_annotation(10,11,3),define_annotation(9,9,2),define_annotation(7,8,1)))
+  returnval = define_dataset(returnval, columnDefinition, "Birnen", list(define_annotation(9,12, 2),define_annotation(8,8,1),define_annotation(1,3, 1)))
+# Aussortiert returnval = define_dataset(returnval, columnDefinition, "Cornichons", list(define_annotation(7,10)))
+  returnval = define_dataset(returnval, columnDefinition, "Erbsen", list(define_annotation(6,7,4),define_annotation(8,8,2),define_annotation(9,10,1)))
+  returnval = define_dataset(returnval, columnDefinition, "Erdbeeren", list(define_annotation(6,6,4),define_annotation(7,7,3),define_annotation(5,5,2),define_annotation(8,12,1),define_annotation(4,4,1)))
+  returnval = define_dataset(returnval, columnDefinition, "Gurken", list(define_annotation(6,8,4),define_annotation(5,5,3),define_annotation(4,4,2),define_annotation(9,9,2),define_annotation(3,3,1),define_annotation(10,10,1)))
+  returnval = define_dataset(returnval, columnDefinition, "Himbeeren", list(define_annotation(7,7,4),define_annotation(6,6,2),define_annotation(8,9,2),define_annotation(5.5,5,1),define_annotation(10,10.5,1)))
+  returnval = define_dataset(returnval, columnDefinition, "Karotten", list(define_annotation(12,12,4),define_annotation(7,11,2),define_annotation(1,6,1)))
+# Aussortiert returnval = define_dataset(returnval, columnDefinition, "Kartoffeln", list(define_annotation(1,12)))
+  returnval = define_dataset(returnval, columnDefinition, "K\u00FCrbissen", list(define_annotation(9,9,4),define_annotation(10,10,3),define_annotation(8,8,2),define_annotation(7,7,1),define_annotation(11,11.5,1)))
   returnval = define_dataset(returnval, columnDefinition, "Orangen", list())
-  returnval = define_dataset(returnval, columnDefinition, "Spargel", list(define_annotation(4,6,main_season),define_annotation(3,3,lame_season)))
-  returnval = define_dataset(returnval, columnDefinition, "Spinat", list(define_annotation(3,5,main_season),define_annotation(9,11,main_season),define_annotation(12,12,lame_season),define_annotation(1,2,lame_season),define_annotation(6,8,lame_season)))
-  returnval = define_dataset(returnval, columnDefinition, "Steinpilzen", list(define_annotation(6,9,main_season),define_annotation(10,11,lame_season)))
-  returnval = define_dataset(returnval, columnDefinition, "Tomaten", list(define_annotation(5,9,main_season),define_annotation(3,4,lame_season),define_annotation(10,11,lame_season)))
+  returnval = define_dataset(returnval, columnDefinition, "Spargel", list(define_annotation(5,5,4),define_annotation(6,6,3),define_annotation(4,4,2),define_annotation(3.5,3,1),define_annotation(7,6.5,1)))
+  returnval = define_dataset(returnval, columnDefinition, "Spinat", list(define_annotation(5,8,4),define_annotation(4,4,3),define_annotation(9,10,3),define_annotation(11,11,2),define_annotation(3,3,1),define_annotation(12,12,1)))
+# Aussortiert returnval = define_dataset(returnval, columnDefinition, "Steinpilzen", list(define_annotation(6,9),define_annotation(10,11)))
+  returnval = define_dataset(returnval, columnDefinition, "Tomaten", list(define_annotation(6,8,4),define_annotation(5,5,3),define_annotation(9,9,3),define_annotation(4,4,2),define_annotation(10,11,2),define_annotation(12,12,1),define_annotation(3,3,1)))
   returnval = define_dataset(returnval, columnDefinition, "Zitronen", list())
-  returnval = define_dataset(returnval, columnDefinition, "Zwiebeln", list(define_annotation(7,10,main_season),define_annotation(11,12,lame_season),define_annotation(1,4,lame_season)))
+  returnval = define_dataset(returnval, columnDefinition, "Zwiebeln", list(define_annotation(8,12,4),define_annotation(1,1,4),define_annotation(2,4,3),define_annotation(5,5,2),define_annotation(6,7,1)))
 
   return(returnval)
 }
